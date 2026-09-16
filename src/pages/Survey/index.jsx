943d029df4c0a708e928ea026f4c83373c909a01
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-16 15:11:10 +0200

    Using API

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/color';
+import { Loader } from '../../utils/style/Atoms';
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`;
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`;
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`;
 
 function Survey() {
   const { questionNumber } = useParams();
@@ -7,18 +37,57 @@ function Survey() {
   const prevQuestionNumber =
     questionNumberInt === 1 ? 1 : questionNumberInt - 1;
   const nextQuestionNumber = questionNumberInt + 1;
+  const [surveyData, setSurveyData] = useState({});
+  const [isDataLoading, setDataLoading] = useState(false);
+
+  // Cette syntaxe permet aussi bien de faire des calls API.
+  // Mais pour utiliser await dans une fonction, il faut que celle-ci soit async (pour asynchrone).
+  // Comme la fonction passée à useEffect ne peut pas être asynchrone,
+  // il faut utiliser une fonction qui est appelée dans useEffect et déclarée en dehors, comme ici 👇.
+  // Essayez de commenter le code créé dans le chapitre et de décommenter fetchData pour voir.
+
+  async function fetchData() {
+    try {
+      const response = await fetch(`http://localhost:8000/survey`);
+      const { surveyData } = await response.json();
+      setSurveyData(surveyData);
+    } catch (err) {
+      console.log('===== error =====', err);
+      setError(true);
+    } finally {
+      setDataLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+    setDataLoading(true);
+    // fetch(`http://localhost:8000/survey`).then((response) =>
+    //   response.json().then(({ surveyData }) => {
+    //     setSurveyData(surveyData);
+    //     setDataLoading(false);
+    //   }),
+    // );
+  }, []);
 
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
-      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-      {questionNumberInt === 10 ? (
-        <Link to="/results">Résultats</Link>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
       ) : (
-        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
       )}
-    </div>
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
+        )}
+      </LinkWrapper>
+    </SurveyContainer>
   );
 }
+
 export default Survey;
