06b912534a34065125c01e62a88c43dc1f06d678
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-16 10:27:25 +0200

    Add defautl proptypes

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,7 +1,42 @@
+import DefaultPicture from '../../assets/profile.png';
 import React from 'react';
+import Card from '../../components/Card';
 
-function Freelance() {
-  return <div>Page des Freelances</div>;
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+    picture: DefaultPicture,
+  },
+  {
+    name: 'Jeanne Biche',
+    jobTitle: 'Développeuse Fullstack',
+    picture: DefaultPicture,
+  },
+  {
+    jobTitle: 'Happiness manager',
+    picture: DefaultPicture,
+  },
+];
+
+function Freelances() {
+  return (
+    <div>
+      <h1>Freelances 👩·💻👨·💻👩·💻</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          picture={profile.picture}
+          title={profile.name}
+        />
+      ))}
+    </div>
+  );
 }
-
-export default Freelance;
+export default Freelances;
