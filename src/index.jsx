f91040720479087f3062f85e8a7d112373006e26
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-17 11:59:32 +0200

    Adding Context

--- src/index.jsx
+++ src/index.jsx
@@ -4,34 +4,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Survey from './pages/Survey';
 import Results from './pages/Results';
-import Freelance from './pages/Freelances';
+import Freelances from './pages/Freelances';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import ClientForm from './components/ClientForm';
 import FreelanceForm from './components/FreelanceForm';
 import Error from './components/Error';
 import styled, { createGlobalStyle } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-  div {
-    font-family: 'Trebuchet MS', Helvetica, sans-serif;
-  }
-`;
+import { ThemeProvider } from './utils/context';
+import GlobalStyle from './utils/style/GlobalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />}>
-          <Route path="client" element={<ClientForm />} />
-          <Route path="freelance" element={<FreelanceForm />} />
-        </Route>
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelance" element={<Freelance />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/freelances" element={<Freelances />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
