4510f82e522a818aa473e24a163fc70bc6bcf6d8
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-18 12:14:06 +0200

    Testing Footer components

---
+++ src/components/Footer/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { ThemeProvider } from '../../utils/context'
+
+describe('Footer', () => {
+    test('Should render without crash', async () =>{
+        render(
+            <ThemeProvider>
+                <Footer/>
+            </ThemeProvider>
+        )
+        const nightModeButton = screen.getByRole('button')
+        expect(nightModeButton.textContent).toBe('Changer de mode : ☀️')
+        fireEvent.click(nightModeButton)
+        expect(nightModeButton.textContent).toBe('Changer de mode : 🌙')
+        // screen.debug()
+    })
+})
