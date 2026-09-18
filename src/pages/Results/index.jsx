6ea88cbf7e924ac222c14f3387f1ff2a714d2de4
Erik THEROUDE <eriktheroude@hotmail.fr>
2026-09-18 11:49:14 +0200

    Testing Result Pages

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -52,7 +52,7 @@ const LoaderWrapper = styled.div`
   justify-content: center;
 `;
 
-function formatFetchParams(answers) {
+export function formatQueryParams(answers) {
   const answerNumbers = Object.keys(answers);
 
   return answerNumbers.reduce((previousParams, answerNumber, index) => {
